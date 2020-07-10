e81aa230fbf60ed049131eed563dcada2cfc07ee
ezluci <68049793+ezluci@users.noreply.github.com>
2020-07-10 20:21:32 +0300

    Update Lumber Tycoon 2 script.lua

--- Lumber Tycoon 2 script.lua
+++ Lumber Tycoon 2 script.lua
@@ -1,22 +1,27 @@
 local Granny = Instance.new("ScreenGui")
+local RemoveTreesGui = Instance.new("Frame")
 local OpenBut = Instance.new("Frame")
 local Open = Instance.new("TextButton")
 local main = Instance.new("Frame")
 local Close = Instance.new("TextButton")
 local eyes = Instance.new("TextButton")
 local Sponge = Instance.new("TextButton")
-local Blood = Instance.new("TextButton")
+local RemoveTrees = Instance.new("TextButton")
 local Money = Instance.new("TextButton")
 local Traps = Instance.new("TextButton")
-local Badges = Instance.new("TextButton")
 local NoClip = Instance.new("TextButton")
-local Upgrade = Instance.new("TextButton")
-local Sword = Instance.new("TextButton")
+local JumpPower = Instance.new("TextButton")
+local JumpPowerOff = Instance.new("TextButton")
+local JumpPowerNum = Instance.new("TextBox")
+local WalkSpeedNum = Instance.new("TextBox")
+local RemoveTreesGui1 = Instance.new("TextLabel")
 local TextLabel5 = Instance.new("TextLabel")
 local TextLabel6 = Instance.new("TextLabel")
+local TextLabel7 = Instance.new("TextLabel")
 local WalkSpeed = Instance.new("TextButton")
 local WalkSpeedOff = Instance.new("TextButton")
 local WalkSpeedBool = true
+local JumpPowerBool = true
 Granny.Name = "Granny"
 Granny.Parent = game.CoreGui
 
@@ -50,6 +55,15 @@ main.Style = Enum.FrameStyle.DropShadow
 main.Visible = false
 main.Active = true
 main.Draggable = true
+
+RemoveTreesGui.Name = "RemoveTreesGui"
+RemoveTreesGui.Parent = Granny
+RemoveTreesGui.BackgroundColor3 = Color3.new(1, 1, 1)
+RemoveTreesGui.Position = UDim2.new(0, 0, 0, 0)
+RemoveTreesGui.Size = UDim2.new(0, 359, 0, 350)
+RemoveTreesGui.Style = Enum.FrameStyle.DropShadow
+RemoveTreesGui.Visible = false
+RemoveTreesGui.Active = true
 
 Close.Name = "Close"
 Close.Parent = main
@@ -94,18 +108,31 @@ Sponge.MouseButton1Down:connect(function()
 game:GetService("ReplicatedStorage").Events.Buy:InvokeServer({["Cost"] = -100, ["Name"] = "SpongeBob Dress", ["Model"] = game:GetService("ReplicatedStorage").Models.Clothes.SpongeBobDress, ["id"] = 3036, ["Rarity"] = 2, ["Icon"] = "rbxassetid://2154049762", ["Typeid"] = 3})
 end)
 
-Blood.Name = "Blood"
-Blood.Parent = main
-Blood.BackgroundColor3 = Color3.new(0, 0, 0)
-Blood.BackgroundTransparency = 0.5
-Blood.Position = UDim2.new(0.583999991, 0, 0.326704532, 0)
-Blood.Size = UDim2.new(0, 142, 0, 32)
-Blood.Font = Enum.Font.SourceSans
-Blood.Text = "Get Blood Chainsaw"
-Blood.TextColor3 = Color3.new(0, 1, 0)
-Blood.TextSize = 14
-Blood.MouseButton1Down:connect(function()
-	local Event = game:GetService("ReplicatedStorage").Events.Buy:InvokeServer({["Rarity"] = 0, ["AnglesXYZ"] = {["Y"] = 0, ["X"] = 0, ["Z"] = 0}, ["id"] = 1014, ["Name"] = "ChainsawBlood", ["Model"] = game:GetService("ReplicatedStorage").Models.Weapons.ChainsawBlood, ["Cost"] = 0, ["CFrameXYZ"] = {["Y"] = 0, ["X"] = 0, ["Z"] = 0}, ["Icon"] = "rbxassetid://1826666844", ["Typeid"] = 1})
+RemoveTrees.Name = "RemoveTrees"
+RemoveTrees.Parent = main
+RemoveTrees.BackgroundColor3 = Color3.new(0, 0, 0)
+RemoveTrees.BackgroundTransparency = 0.5
+RemoveTrees.Position = UDim2.new(0.583999991, 0, 0.326704532, 0)
+RemoveTrees.Size = UDim2.new(0, 142, 0, 32)
+RemoveTrees.Font = Enum.Font.SourceSans
+RemoveTrees.Text = "Remove all the trees (cfi)"
+RemoveTrees.TextColor3 = Color3.new(0, 1, 0)
+RemoveTrees.TextSize = 14
+RemoveTrees.MouseButton1Down:connect(function()
+    RemoveTreesGui.Visible = true
+  --  for i,v in pairs(game.Workspace:GetDescendants()) do
+	--    if v.Name == "WoodSection" and v.Parent:FindFirstChild("CutEvent") then
+	--        game.ReplicatedStorage.Interaction.ClientIsDragging:FireServer(v.Parent)
+	--        game.ReplicatedStorage.Interaction.DestroyStructure:FireServer(v.Parent)
+	--    end
+--	end
+--	game.Workspace.DescendantAdded:connect(function(Thing)
+--	    wait(0.1)
+--	    if Thing.Name == "WoodSection" and Thing.Parent:FindFirstChild("CutEvent") then
+	--        game.ReplicatedStorage.Interaction.ClientIsDragging:FireServer(Thing.Parent)
+	--        game.ReplicatedStorage.Interaction.DestroyStructure:FireServer(Thing.Parent)
+	--    end
+--	end)
 end)
 
 Money.Name = "Money"
@@ -122,6 +149,17 @@ Money.MouseButton1Down:connect(function()
 game:GetService("ReplicatedStorage").Events.Winner:FireServer(9999999999)
 end)
 
+RemoveTreesGui1.Name = "RemoveTreesGui1"
+RemoveTreesGui1.Parent = RemoveTreesGui
+RemoveTreesGui1.BackgroundColor3 = Color3.new(0, 0, 0)
+RemoveTreesGui1.BackgroundTransparency = 0.89999997615814
+RemoveTreesGui1.Position = UDim2.new(0.2, 0, 0.83, 0)
+RemoveTreesGui1.Size = UDim2.new(0, 200, 0, 17)
+RemoveTreesGui1.Font = Enum.Font.SourceSans
+RemoveTreesGui1.Text = "wv3t7w9kny8ji"
+RemoveTreesGui1.TextColor3 = Color3.new(0, 1, 0)
+RemoveTreesGui1.TextSize = 14
+
 Traps.Name = "Traps"
 Traps.Parent = main
 Traps.BackgroundColor3 = Color3.new(0, 0, 0)
@@ -136,47 +174,11 @@ Traps.MouseButton1Down:connect(function()
 	game:GetService("ReplicatedStorage").Events.PutTrap:FireServer(game.Players.LocalPlayer)
 end)
 
-Badges.Name = "Badges"
-Badges.Parent = main
-Badges.BackgroundColor3 = Color3.new(0, 0, 0)
-Badges.BackgroundTransparency = 0.5
-Badges.Position = UDim2.new(0.0346666649, 0, 0.326704532, 0)
-Badges.Size = UDim2.new(0, 142, 0, 32)
-Badges.Font = Enum.Font.SourceSans
-Badges.Text = "All Badges"
-Badges.TextColor3 = Color3.new(0, 1, 0)
-Badges.TextSize = 14
-Badges.MouseButton1Down:connect(function()
-game:GetService("ReplicatedStorage").Events.Badge:FireServer(1866050915)
-wait(0.1)
-game:GetService("ReplicatedStorage").Events.Badge:FireServer(1866066746)
-wait(0.1)
-game:GetService("ReplicatedStorage").Events.Badge:FireServer(1866085759)
-wait(0.1)
-game:GetService("ReplicatedStorage").Events.Badge:FireServer(1867767796)
-wait(0.1)
-game:GetService("ReplicatedStorage").Events.Badge:FireServer(1867781859)
-wait(0.1)
-game:GetService("ReplicatedStorage").Events.Badge:FireServer(2043875698)
-wait(0.1)
-game:GetService("ReplicatedStorage").Events.Badge:FireServer(2043881344)
-wait(0.1)
-game:GetService("ReplicatedStorage").Events.Badge:FireServer(2108429736)
-wait(0.1)
-game:GetService("ReplicatedStorage").Events.Badge:FireServer(2124422321)
-wait(0.1)
-game:GetService("ReplicatedStorage").Events.Badge:FireServer(2124422322)
-wait(0.1)
-game:GetService("ReplicatedStorage").Events.Badge:FireServer(2124422326)
-wait(0.1)
-game:GetService("ReplicatedStorage").Events.Badge:FireServer(2124422327)
-end)
-
 NoClip.Name = "No Clip"
 NoClip.Parent = main
 NoClip.BackgroundColor3 = Color3.new(0, 0, 0)
 NoClip.BackgroundTransparency = 0.5
-NoClip.Position = UDim2.new(0.0346666649, 0, 0.448863626, 0)
+NoClip.Position = UDim2.new(0.0346666649, 0, 0.326704532, 0)
 NoClip.Size = UDim2.new(0, 142, 0, 32)
 NoClip.Font = Enum.Font.SourceSans
 NoClip.Text = "No Clip"
@@ -191,44 +193,71 @@ end
 end)
 end)
 
-Upgrade.Name = "Upgrade"
-Upgrade.Parent = main
-Upgrade.BackgroundColor3 = Color3.new(0, 0, 0)
-Upgrade.BackgroundTransparency = 0.5
-Upgrade.Position = UDim2.new(0.0357819796, 0, 0.574953258, 0)
-Upgrade.Size = UDim2.new(0, 142, 0, 32)
-Upgrade.Font = Enum.Font.SourceSans
-Upgrade.Text = "Upgrade to max"
-Upgrade.TextColor3 = Color3.new(0, 1, 0)
-Upgrade.TextSize = 14
-Upgrade.MouseButton1Down:connect(function()
-local RSS = game:service('ReplicatedStorage')
-local uP = RSS.Events.upPoint
-local plr = game:service('Players').LocalPlayer
+JumpPowerNum.Name = "JumpPowerNum"
+JumpPowerNum.Parent = main
+JumpPowerNum.BackgroundColor3 = Color3.new(0, 0, 0)
+JumpPowerNum.BackgroundTransparency = 0.5
+JumpPowerNum.Position = UDim2.new(0.0346666649, 0, 0.448863626,0)
+JumpPowerNum.Size = UDim2.new(0, 142, 0, 32)
+JumpPowerNum.Font = Enum.Font.SourceSans
+JumpPowerNum.Text = "100"
+JumpPowerNum.TextColor3 = Color3.new(0, 0, 1)
+JumpPowerNum.TextSize = 14
 
-for i = 1, 1000 do
-uP:FireServer(plr.Upgrades.Player.Speed, 1)
-uP:FireServer(plr.Upgrades.Player.RemoveTrap, 1)
-uP:FireServer(plr.Upgrades.Player.Revive, 1)
-
-uP:FireServer(plr.Upgrades.Granny.Speed, 1)
-uP:FireServer(plr.Upgrades.Player.PutTrap, 1)
-end
+JumpPower.Name = "JumpPower"
+JumpPower.Parent = main
+JumpPower.BackgroundColor3 = Color3.new(0, 0, 0)
+JumpPower.BackgroundTransparency = 0.5
+JumpPower.Position = UDim2.new(0.0357819796, 0, 0.574953258, 0)
+JumpPower.Size = UDim2.new(0, 142, 0, 32)
+JumpPower.Font = Enum.Font.SourceSans
+JumpPower.Text = "Jump Power - OFF"
+JumpPower.TextColor3 = Color3.new(0, 1, 0)
+JumpPower.TextSize = 14
+JumpPower.MouseButton1Down:connect(function()
+    JumpPowerBool = false
+    wait(0.25)
+    JumpPowerBool = true
+    JumpPower.Visible = false
+    JumpPowerOff.Visible = true
+    while true do
+        if not JumpPowerBool then
+            game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+            return end
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = JumpPowerNum.Text
+        wait()
+    end
 end)
 
-Sword.Name = "Sword"
-Sword.Parent = main
-Sword.BackgroundColor3 = Color3.new(0, 0, 0)
-Sword.BackgroundTransparency = 0.5
-Sword.Position = UDim2.new(0.588619411, 0, 0.451672852, 0)
-Sword.Size = UDim2.new(0, 142, 0, 32)
-Sword.Font = Enum.Font.SourceSans
-Sword.Text = "Get Gold Sword"
-Sword.TextColor3 = Color3.new(0, 1, 0)
-Sword.TextSize = 14
-Sword.MouseButton1Down:connect(function()
-local Event = game:GetService("ReplicatedStorage").Events.Buy:InvokeServer({["Rarity"] = 0, ["AnglesXYZ"] = {["Y"] = 0, ["X"] = 0, ["Z"] = 0}, ["id"] = 1013, ["Name"] = "GoldSword", ["Model"] = game:GetService("ReplicatedStorage").Models.Weapons.GoldSword, ["Cost"] = 0, ["CFrameXYZ"] = {["Y"] = 0, ["X"] = 0, ["Z"] = 0}, ["Icon"] = "rbxassetid://1826666844", ["Typeid"] = 1})
+JumpPowerOff.Visible = false
+JumpPowerOff.Name = "JumpPowerOff"
+JumpPowerOff.Parent = main
+JumpPowerOff.BackgroundColor3 = Color3.new(0, 0, 0)
+JumpPowerOff.BackgroundTransparency = 0.5
+JumpPowerOff.Position = UDim2.new(0.0357819796, 0, 0.574953258, 0)
+JumpPowerOff.Size = UDim2.new(0, 142, 0, 32)
+JumpPowerOff.Font = Enum.Font.SourceSans
+JumpPowerOff.Text = "Jump Power - ON"
+JumpPowerOff.TextColor3 = Color3.new(0, 1, 0)
+JumpPowerOff.TextSize = 14
+JumpPowerOff.MouseButton1Down:connect(function()
+    JumpPowerOff.Visible = false
+    JumpPower.Visible = true
+    JumpPowerBool = false
+    wait(0.25)
+    JumpPowerBool = true
 end)
+--
+WalkSpeedNum.Name = "WalkSpeedNum"
+WalkSpeedNum.Parent = main
+WalkSpeedNum.BackgroundColor3 = Color3.new(0, 0, 0)
+WalkSpeedNum.BackgroundTransparency = 0.5
+WalkSpeedNum.Position = UDim2.new(0.588619411, 0, 0.451672852, 0)
+WalkSpeedNum.Size = UDim2.new(0, 142, 0, 32)
+WalkSpeedNum.Font = Enum.Font.SourceSans
+WalkSpeedNum.Text = "40"
+WalkSpeedNum.TextColor3 = Color3.new(0, 0, 1)
+WalkSpeedNum.TextSize = 14
 
 TextLabel5.Name = "TextLabel5"
 TextLabel5.Parent = main
@@ -252,6 +281,17 @@ TextLabel6.Text = "Please wait atleast 1 second before pressing the buttons bc i
 TextLabel6.TextColor3 = Color3.new(1, 1, 0)
 TextLabel6.TextSize = 14
 
+TextLabel7.Name = "TextLabel7"
+TextLabel7.Parent = main
+TextLabel7.BackgroundColor3 = Color3.new(0, 0, 0)
+TextLabel7.BackgroundTransparency = 0.89999997615814
+TextLabel7.Position = UDim2.new(0.2, 0, 0.91, 0)
+TextLabel7.Size = UDim2.new(0, 200, 0, 17)
+TextLabel7.Font = Enum.Font.SourceSans
+TextLabel7.Text = "cfi = Click for info"
+TextLabel7.TextColor3 = Color3.new(1, 0, 0)
+TextLabel7.TextSize = 16
+
 WalkSpeed.Name = "WalkSpeed"
 WalkSpeed.Parent = main
 WalkSpeed.BackgroundColor3 = Color3.new(0, 0, 0)
@@ -263,13 +303,16 @@ WalkSpeed.Text = "Walk speed - OFF"
 WalkSpeed.TextColor3 = Color3.new(0, 1, 0)
 WalkSpeed.TextSize = 14
 WalkSpeed.MouseButton1Down:connect(function()
+    WalkSpeedBool = false
+    wait(0.25)
+    WalkSpeedBool = true
     WalkSpeed.Visible = false
     WalkSpeedOff.Visible = true
     while true do
         if not WalkSpeedBool then
             game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
             return end
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 40
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = WalkSpeedNum.Text
         wait()
     end
 end)
@@ -288,7 +331,7 @@ WalkSpeedOff.TextSize = 14
 WalkSpeedOff.MouseButton1Down:connect(function()
     WalkSpeedOff.Visible = false
     WalkSpeed.Visible = true
-WalkSpeedBool = false
-wait(1)
-WalkSpeedBool = true
+    WalkSpeedBool = false
+    wait(0.25)
+    WalkSpeedBool = true
 end)
