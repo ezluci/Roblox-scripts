c62175eea9307e8a823f35deff9f3fe44ce0c969
ezluci <ezluci1@gmail.com>
2020-08-24 11:29:16 +0300

    Update Script Lumber Tycoon 2.lua

--- Script Lumber Tycoon 2.lua
+++ Script Lumber Tycoon 2.lua
@@ -1,6 +1,7 @@
 --BORING THINGS
 local OK = Instance.new("ScreenGui")
 local Main = Instance.new("Frame")
+local MainClose = Instance.new("TextButton")
 local OpenBut = Instance.new("TextButton")
 local WalkSpeedOff = Instance.new("TextButton")
 
@@ -11,11 +12,17 @@ OK.Parent = game.CoreGui
 
 Main.Name = "Main"
 Main.Parent = OK
-Main.BackgroundColor3 = Color3.new(0, 0.5, 1)
+Main.BackgroundColor3 = Color3.new(0, 0, 0)
+Main.BackgroundTransparency = 0.5
 Main.Position = UDim2.new(0, 0, 1, 0)
 Main.Size = UDim2.new(0, 500, 0, 600)
 Main.Visible = false
 Main.Draggable = true
+Main.Active = true
+
+MainClose.Name = "MainClose"
+MainClose.Parent = Main
+MainClose.BackgroundColor3 = 
 
 OpenBut.Name = "OpenBut"
 OpenBut.Parent = OK
