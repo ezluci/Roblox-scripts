a3b0e05b6774a55fe1e338c6f7006d48019c4460
ezluci <ezluci1@gmail.com>
2020-08-27 17:58:31 +0300

    Update Script Lumber Tycoon 2.lua

--- Script Lumber Tycoon 2.lua
+++ Script Lumber Tycoon 2.lua
@@ -25,9 +25,7 @@ local vnau1MenuJumpPowerOn = Instance.new("TextButton")
 local vnau1MenuJumpPowerNumber = Instance.new("TextBox")
 local vnau1MenuNoClipOff = Instance.new("TextButton")
 local vnau1MenuNoClipOn = Instance.new("TextButton")
-local vnau1MenuFlyOff = Instance.new("TextButton")
-local vnau1MenuFlyOn = Instance.new("TextButton")
-local vnau1MenuFlyRealOn = Instance.new("TextButton")
+local vnau1MenuFly = Instance.new("TextButton")
 --local vnau2Menu = Instance.new("Frame")
 --local vnau3Menu = Instance.new("Frame")
 --local vnau4Menu = Instance.new("Frame")
@@ -312,40 +310,16 @@ vnau1MenuNoClipOn.TextColor3 = Color3.new(1, 1, 1)
 vnau1MenuNoClipOn.TextSize = 16
 vnau1MenuNoClipOn.Visible = false
 
-vnau1MenuFlyOff.Name = "vnau1MenuFlyOff"
-vnau1MenuFlyOff.Parent = vnau1Menu
-vnau1MenuFlyOff.BackgroundColor3 = Color3.new(0.18039215, 0.85882352, 0.85098039)
-vnau1MenuFlyOff.BackgroundTransparency = 0.5
-vnau1MenuFlyOff.Position = UDim2.new(0, 0, 0.75, 0)
-vnau1MenuFlyOff.Size = UDim2.new(0, 300, 0, 58.5)
-vnau1MenuFlyOff.Font = Enum.Font.SciFi
-vnau1MenuFlyOff.Text = "fly - off"
-vnau1MenuFlyOff.TextColor3 = Color3.new(1, 1, 1)
-vnau1MenuFlyOff.TextSize = 16
-
-vnau1MenuFlyOn.Name = "vnau1MenuFlyOn"
-vnau1MenuFlyOn.Parent = vnau1Menu
-vnau1MenuFlyOn.BackgroundColor3 = Color3.new(0.18039215, 0.85882352, 0.85098039)
-vnau1MenuFlyOn.BackgroundTransparency = 0.5
-vnau1MenuFlyOn.Position = UDim2.new(0, 0, 0.75, 0)
-vnau1MenuFlyOn.Size = UDim2.new(0, 300, 0, 58.5)
-vnau1MenuFlyOn.Font = Enum.Font.SciFi
-vnau1MenuFlyOn.Text = "fly - on (press Q - off)"
-vnau1MenuFlyOn.TextColor3 = Color3.new(1, 1, 1)
-vnau1MenuFlyOn.TextSize = 16
-vnau1MenuFlyOn.Visible = false
-
-vnau1MenuFlyRealOn.Name = "vnau1MenuFlyRealOn"
-vnau1MenuFlyRealOn.Parent = vnau1Menu
-vnau1MenuFlyRealOn.BackgroundColor3 = Color3.new(0.18039215, 0.85882352, 0.85098039)
-vnau1MenuFlyRealOn.BackgroundTransparency = 0.5
-vnau1MenuFlyRealOn.Position = UDim2.new(0, 0, 0.75, 0)
-vnau1MenuFlyRealOn.Size = UDim2.new(0, 300, 0, 58.5)
-vnau1MenuFlyRealOn.Font = Enum.Font.SciFi
-vnau1MenuFlyRealOn.Text = "fly - on (press Q - on)"
-vnau1MenuFlyRealOn.TextColor3 = Color3.new(1, 1, 1)
-vnau1MenuFlyRealOn.TextSize = 16
-vnau1MenuFlyRealOn.Visible = false
+vnau1MenuFly.Name = "vnau1MenuFly"
+vnau1MenuFly.Parent = vnau1Menu
+vnau1MenuFly.BackgroundColor3 = Color3.new(0.18039215, 0.85882352, 0.85098039)
+vnau1MenuFly.BackgroundTransparency = 0.5
+vnau1MenuFly.Position = UDim2.new(0, 0, 0.75, 0)
+vnau1MenuFly.Size = UDim2.new(0, 300, 0, 58.5)
+vnau1MenuFly.Font = Enum.Font.SciFi
+vnau1MenuFly.Text = "fly (toggle with Q)"
+vnau1MenuFly.TextColor3 = Color3.new(1, 1, 1)
+vnau1MenuFly.TextSize = 16
 
 
 --MOUSE1CLICK EVENTS
@@ -571,9 +545,87 @@ vnau1MenuNoClipOn.MouseButton1Down:Connect (
     end
 )
 
-vnau1MenuFlyOff.MouseButton1Down:Connect (
+vnau1MenuFly.MouseButton1Down:Connect (
     function()
-        vnau1MenuFlyOff.Visible = false
-        vnau1MenuFlyOn.Visible = true
+        repeat wait()
+        until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Torso") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid")
+        local mouse = game.Players.LocalPlayer:GetMouse()
+        repeat wait() until mouse
+        local plr = game.Players.LocalPlayer
+        local torso = plr.Character.Torso
+        local flying = true
+        local deb = true
+        local ctrl = {f = 0, b = 0, l = 0, r = 0}
+        local lastctrl = {f = 0, b = 0, l = 0, r = 0}
+        local maxspeed = 50
+        local speed = 0
+        
+        function Fly()
+        local bg = Instance.new("BodyGyro", torso)
+        bg.P = 9e4
+        bg.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+        bg.cframe = torso.CFrame
+        local bv = Instance.new("BodyVelocity", torso)
+        bv.velocity = Vector3.new(0,0.1,0)
+        bv.maxForce = Vector3.new(9e9, 9e9, 9e9)
+        repeat wait()
+        plr.Character.Humanoid.PlatformStand = true
+        if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then
+        speed = speed+.5+(speed/maxspeed)
+        if speed > maxspeed then
+        speed = maxspeed
+        end
+        elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then
+        speed = speed-1
+        if speed < 0 then
+        speed = 0
+        end
+        end
+        if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then
+        bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+        lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r}
+        elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then
+        bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed
+        else
+        bv.velocity = Vector3.new(0,0.1,0)
+        end
+        bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0)
+        until not flying
+        ctrl = {f = 0, b = 0, l = 0, r = 0}
+        lastctrl = {f = 0, b = 0, l = 0, r = 0}
+        speed = 0
+        bg:Destroy()
+        bv:Destroy()
+        plr.Character.Humanoid.PlatformStand = false
+        end
+        mouse.KeyDown:connect(function(key)
+        if key:lower() == "q" then
+        if flying then flying = false
+        else
+        flying = true
+        Fly()
+        end
+        elseif key:lower() == "w" then
+        ctrl.f = 1
+        elseif key:lower() == "s" then
+        ctrl.b = -1
+        elseif key:lower() == "a" then
+        ctrl.l = -1
+        elseif key:lower() == "d" then
+        ctrl.r = 1
+        end
+        end)
+        mouse.KeyUp:connect(function(key)
+        if key:lower() == "w" then
+        ctrl.f = 0
+        elseif key:lower() == "s" then
+        ctrl.b = 0
+        elseif key:lower() == "a" then
+        ctrl.l = 0
+        elseif key:lower() == "d" then
+        ctrl.r = 0
+        end
+        end)
+        Fly()
     end
 )
